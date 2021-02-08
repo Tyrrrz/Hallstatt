14ca8fe99a21cd817e92396fc7f7586fbf84a12c
Tyrrrz <tyrrrz@gmail.com>
2021-02-09 00:26:49 +0200

    Rename Internal to Utils

--- Hallstatt/TestController.cs
+++ Hallstatt/TestController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
-using Hallstatt.Internal;
+using Hallstatt.Utils;
 
 namespace Hallstatt
 {
--- Hallstatt/Utils/AsyncExtensions.cs
+++ Hallstatt/Utils/AsyncExtensions.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Threading.Tasks;
 
-namespace Hallstatt.Internal
+namespace Hallstatt.Utils
 {
     internal static class AsyncExtensions
     {
